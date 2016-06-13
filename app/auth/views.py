18097b43a8d4c8495b6753f4e13bb83c90f36f0b
fengwutianya <949979029@qq.com>
2016-06-13 16:20:27 +0800

    user login module

--- app/auth/views.py
+++ app/auth/views.py
@@ -36,7 +36,7 @@ def register():
     if form.validate_on_submit():
         user = User(
             email=form.email.data,
-            username=form.email.data,
+            username=form.username.data,
             password=form.password.data
         )
         db.session.add(user)
